bf966ec3ee5d7935c0d2d29a81282aaefcb2f76c
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-01-21 05:47:38 +0530

    your list implementation with dummy data

--- src/Home/MyList/MyList.tsx
+++ src/Home/MyList/MyList.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
+import ListItem from "../Search/ListView/ListItem";
+import { ApiListItemProps } from "../Search/ListView/ListView";
+
+import { dummyData as wholeData } from "./dummyData";
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
+    backgroundColor: "white",
   },
 });
 const MyList = () => {
+  const limitedData = Object.values(wholeData).map(
+    (value, _): ApiListItemProps => {
+      const rtn = {
+        cast: value.cast.map(({ actor }) => actor),
+        title: value.title,
+        idbRating: value.rating,
+        poster: value.poster,
+        id: (Math.random() * 100000).toString(),
+      };
+      return rtn;
+    }
+  );
+  const api = limitedData;
+
   return (
     <View style={style.container}>
-      <Text>My List</Text>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <View style={{ flex: 1, marginTop: 40 }} />
+          </>
+        }
+        data={api}
+        renderItem={({ item, index }) => (
+          <ListItem key={index} onPress={() => true} {...item} />
+        )}
+      />
     </View>
   );
 };
